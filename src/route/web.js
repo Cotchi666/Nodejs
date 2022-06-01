41a289cb05bad87a448cfcf9a40fe4201616cf47
Nguyễn Ngọc Chiến <Chien@DESKTOP-SCBOTE.(none)>
2022-06-01 21:08:48 +0700

    setup mvc homepage

--- src/route/web.js
+++ src/route/web.js
@@ -5,11 +5,11 @@ import homeController from "../controllers/homeController";
 let router = express.Router();
 
 let initWebRoutes = (app) => {
-    router.get('/',(req, res)=>{
-        return res.send('hello with Chien')
-    });
-    // router.get('/', homeController.getHomePage);
-    // router.get('/about', homeController.getAboutPage);
+    // router.get('/',(req, res)=>{
+    //     return res.send('hello with Chien')
+    // });
+    router.get('/', homeController.getHomePage);
+    router.get('/about', homeController.getAboutPage);
    
     return app.use("/", router);
 }
