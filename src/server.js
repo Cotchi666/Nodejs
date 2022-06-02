3e1c0e7b0dfd24f2affcffcf5e1d035241944c93
Nguyễn Ngọc Chiến <Chien@DESKTOP-SCBOTE.(none)>
2022-06-02 08:32:53 +0700

    data pushes

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ import bodyParser from "body-parser";
 //lay tham so tu client (query params vd /user?id=7)
 import viewEngine from "./config/viewEngine";
 import initWebRoutes from './route/web';
-
+import connectDB from "./config/connectDB"
 require('dotenv').config();//goi den ham cofig cua env de chay (process.env.PORT)
 
 let app = express();
@@ -15,6 +15,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 viewEngine(app);
 initWebRoutes(app);
+connectDB();
 
 let port = process.env.PORT || 3000;
 //Port === undefined => port = 3000
