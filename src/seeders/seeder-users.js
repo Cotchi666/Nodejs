b2d37efece72b42f449bcbc11512ee6a92bcb945
Nguyễn Ngọc Chiến <Chien@DESKTOP-SCBOTE.(none)>
2022-06-04 09:29:44 +0700

    data create

---
+++ src/seeders/seeder-users.js
@@ -0,0 +1,26 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('Users', [{
+      email: 'chiennguyen.170901@gmail.com',
+      password: '123456',
+      firstName: 'Nguyen',
+      lastName: 'Chien',
+      address: 'HCM',
+      gender: 1,
+      typeRole: 'ROLE',
+      keyRole: 'R1',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }]);
+  },
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
