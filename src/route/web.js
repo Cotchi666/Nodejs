ca3fb491edec03c1fcb2e2ce590fb3f8744fd8a8
Nguyễn Ngọc Chiến <Chien@DESKTOP-SCBOTE.(none)>
2022-06-05 22:36:38 +0700

    update

--- src/route/web.js
+++ src/route/web.js
@@ -10,8 +10,11 @@ let initWebRoutes = (app) => {
     // });
     router.get('/', homeController.getHomePage);
     router.get('/about', homeController.getAboutPage);
-   router.post('/post-crud', homeController.postCRUD);
-   router.get('/get-crud', homeController.getCRUD);
+    router.post('/post-crud', homeController.postCRUD);
+    router.get('/get-crud', homeController.getCRUD);
+    router.get('/edit-crud', homeController.getEditCRUD);
+    router.post('/put-crud', homeController.putCRUD);
+    
     return app.use("/", router);
 }
 
